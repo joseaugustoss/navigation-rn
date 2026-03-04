df2d096cf99f27e7771dd3704f4b3e434944add9
José Augusto <joseaugusto0408@gmail.com>
2026-03-04 08:27:26 -0300

    home

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import type React from "react";
+import { View } from "react-native";
+
+export function Header({ children }: { children: React.ReactNode }) {
+  return (
+    <View
+      style={{
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginBottom: 54,
+      }}
+    >
+      {children}
+    </View>
+  );
+}
