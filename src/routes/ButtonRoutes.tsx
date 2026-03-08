d252e17782a0d3281d2179b462f9ffb404f02c57
José Augusto <joseaugusto0408@gmail.com>
2026-03-08 14:13:14 -0300

    Bottom_Drawer e Aninhadas - Customizando bottom navigator

--- src/routes/ButtonRoutes.tsx
+++ src/routes/ButtonRoutes.tsx
@@ -5,6 +5,7 @@ import {
 
 import { Home } from "@/app/Home";
 import { Product } from "@/app/Product";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export type BottomRoutesList = {
   home: undefined;
@@ -17,10 +18,33 @@ export function BottomRoutes() {
   return (
     <Tab.Navigator
       initialRouteName="home"
-      screenOptions={{ headerShown: false }}
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#2c46b1",
+        tabBarInactiveTintColor: "#444444",
+        tabBarLabelPosition: "beside-icon",
+      }}
     >
-      <Tab.Screen name="home" component={Home} />
-      <Tab.Screen name="product" component={Product} />
+      <Tab.Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarLabel: "Ínicio",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="product"
+        component={Product}
+        options={{
+          tabBarLabel: "Produto",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="add-circle" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
