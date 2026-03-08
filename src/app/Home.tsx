149c04404a86caf893432c493b97fee55e568740
José Augusto <joseaugusto0408@gmail.com>
2026-03-08 15:14:54 -0300

    toggleDrawer

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -12,6 +12,7 @@ export function Home({ navigation }: DrawerRoutesProps<"home">) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
+        <ButtonIcon name="menu" onPress={() => navigation.toggleDrawer()} />
         <Title>Home</Title>
         <ButtonIcon
           name="add-circle"
